4e67bfe74aea065b2a1ad11077b61053e422931e
Dan Kingswell <dankingswell22@gmail.com>
2018-08-23 19:13:05 +0100

    adjusted webpack config for production added separate css source maps and files added links from html

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,52 @@
-const path = require("path")
+    const path = require("path")
+    const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 
-module.exports = {
-    mode:"development",
-    entry:"./src/app.js",
-    output : {
-        path: path.join(__dirname,"public"),
-        filename:"bundle.js"
-    },
-    module:
-        {  
-            rules:[
-                { 
-                    loader:"babel-loader",
-                    test:/\.js$/,
-                    exclude:/node_modules/
+    module.exports = (env) => {
+        console.log("env ", env)
+       const isProduction = env === "production"  
+
+       return{ 
+
+            mode:"development",
+            entry:"./src/app.js",
+            output : {
+                path: path.join(__dirname,"public"),
+                filename:"bundle.js"
+            },
+            module:
+                {  
+                    rules:[
+                        { 
+                            loader:"babel-loader",
+                            test:/\.js$/,
+                            exclude:/node_modules/
+                        },
+                        {
+                            test:/\.s?css/,
+                            use: [
+                                MiniCssExtractPlugin.loader,
+                                    {loader:"css-loader",options:{
+                                        sourceMap: true
+                                        }
+                                    },
+                                    { loader: "sass-loader",options:{
+                                        sourceMap:true
+                                    }
+                                    }
+
+                            ]
+                        }
+                    ]
+                
                 },
-                {
-                    test:/\.s?css/,
-                    use:[
-                        "style-loader",
-                        "css-loader",
-                        "sass-loader"
-                    ] 
-                }
-            ]
-        
+                plugins:[
+                    new MiniCssExtractPlugin({filename:"styles.css"})
+                ],
+            devtool:isProduction ? "source-map" : "inline-source-map",
+            devServer:{
+                contentBase: path.join(__dirname,"public"),
+                historyApiFallback:true
+            }
         }
-    ,
-    devtool:"cheap-module-eval-source-map",
-    devServer:{
-        contentBase: path.join(__dirname,"public"),
-        historyApiFallback:true
-    }
+}
 
-};
